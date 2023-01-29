3d813d6aaaa19b542da85101a783498c1f6d91a6
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-01-28 21:22:29 -0600

    Update Quote-Scraper.py

--- Quote-Scraper.py
+++ Quote-Scraper.py
@@ -1,22 +1,28 @@
 import random
+import requests
 from bs4 import BeautifulSoup
 import csv
 
 URL = "http://quotes.toscrape.com"
-page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, 'html.parser')
-quotes = soup.find_all(class_='quote')
+try:
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    quotes = soup.find_all(class_='quote')
+    
+    def get_random_quote():
+        quote = random.choice(quotes)
+        return quote.find(class_='text').get_text() + ' - ' + quote.find(class_='author').get_text()
+    
+    def save_to_csv(quote):
+        with open('quotes.csv', mode='a') as file:
+            writer = csv.writer(file)
+            writer.writerow([quote])
 
-def get_random_quote():
-quote = random.choice(quotes)
-return quote.find(class_='text').get_text() + ' - ' + quote.find(class_='author').get_text()
-
-def save_to_csv(quote):
-with open('quotes.csv', mode='a') as file:
-writer = csv.writer(file)
-writer.writerow([quote])
-
-random_quote = get_random_quote()
-print(random_quote)
-save_to_csv(random_quote)
+    random_quote = get_random_quote()
+    print(random_quote)
+    save_to_csv(random_quote)
+except requests.exceptions.RequestException as e:
+    print(f"Request failed: {e}")
+except Exception as e:
+    print(f"An error occurred: {e}")
