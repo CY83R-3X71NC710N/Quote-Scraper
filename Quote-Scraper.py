f7d7d682bf54108aedc51d9b7e768fba67a635a2
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-01-29 20:54:18 -0600

    Update Quote-Scraper.py

--- Quote-Scraper.py
+++ Quote-Scraper.py
@@ -5,20 +5,24 @@ import csv
 
 URL = "http://quotes.toscrape.com"
 
-try:
+def get_random_quote():
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
     quotes = soup.find_all(class_='quote')
-    
-    def get_random_quote():
-        quote = random.choice(quotes)
-        return quote.find(class_='text').get_text() + ' - ' + quote.find(class_='author').get_text()
-    
-    def save_to_csv(quote):
-        with open('quotes.csv', mode='a') as file:
-            writer = csv.writer(file)
-            writer.writerow([quote])
+    quote = random.choice(quotes)
+    return quote.find(class_='text').get_text() + ' - ' + quote.find(class_='author').get_text()
 
+def save_to_csv(quote):
+    with open('quotes.csv', mode='r') as file:
+        reader = csv.reader(file)
+        quotes_in_file = [row[0] for row in reader]
+    if quote in quotes_in_file:
+        return
+    with open('quotes.csv', mode='a') as file:
+        writer = csv.writer(file)
+        writer.writerow([quote])
+
+try:
     random_quote = get_random_quote()
     print(random_quote)
     save_to_csv(random_quote)
