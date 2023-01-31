5d6fd935329a9889080e94d815d1c5c0b97bbaa8
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-01-31 16:38:11 -0600

    Update Quote-Scraper.py

--- Quote-Scraper.py
+++ Quote-Scraper.py
@@ -1,38 +1,22 @@
 import random
-import requests
 from bs4 import BeautifulSoup
 import csv
 
 URL = "http://quotes.toscrape.com"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+quotes = soup.find_all(class_='quote')
 
 def get_random_quote():
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    quotes = soup.find_all(class_='quote')
-    quote = random.choice(quotes)
-    return quote.find(class_='text').get_text() + ' - ' + quote.find(class_='author').get_text()
+quote = random.choice(quotes)
+return quote.find(class_='text').get_text() + ' - ' + quote.find(class_='author').get_text()
 
-def save_to_csv(quote, recursion_limit=5):
-    with open('quotes.csv', mode='r') as file:
-        reader = csv.reader(file)
-        quotes_in_file = [row[0] for row in reader]
-    if quote in quotes_in_file:
-        recursion_limit -= 1
-        if recursion_limit == 0:
-            raise Exception("Recursion limit reached")
-        return False
-    with open('quotes.csv', mode='a') as file:
-        writer = csv.writer(file)
-        writer.writerow([quote])
-    return True
+def save_to_csv(quote):
+with open('quotes.csv', mode='a') as file:
+writer = csv.writer(file)
+writer.writerow([quote])
 
-while True:
-    try:
-        random_quote = get_random_quote()
-        success = save_to_csv(random_quote)
-        if success:
-            break
-    except requests.exceptions.RequestException as e:
-        print(f"Request failed: {e}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+random_quote = get_random_quote()
+print(random_quote)
+save_to_csv(random_quote)
